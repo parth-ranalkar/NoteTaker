f64acd7f5c52ff503467778757f26b17e574b066
unknown <parthranalkar28@gmail.com>
2022-12-17 17:47:57 +0530

    final commit

---
+++ src/main/java/com/entities/Note.java
@@ -0,0 +1,66 @@
+package com.entities;
+
+import java.time.LocalDate;
+import java.util.Random;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "notes")
+public class Note {
+	@Id
+	private int id;
+	private String title;
+	@Column(length = 1500)
+	private String content;
+	private LocalDate addedDate;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public LocalDate getAddedDate() {
+		return addedDate;
+	}
+
+	public void setAddedDate(LocalDate addedDate) {
+		this.addedDate = addedDate;
+	}
+
+	public Note(String title, String content, LocalDate addedDate) {
+		super();
+		this.id = new Random().nextInt(100000);
+		this.title = title;
+		this.content = content;
+		this.addedDate = addedDate;
+	}
+
+	public Note() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+}
